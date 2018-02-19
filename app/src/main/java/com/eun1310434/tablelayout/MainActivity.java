73e714bab7c171813d98acfd030830593ea01ac4
eun1310434 <eun1310434@naver.com>
2018-02-19 20:05:11 +0900

    Initial commit

---
+++ app/src/main/java/com/eun1310434/tablelayout/MainActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 19.02.2018
+ * eun1310434@naver.com
+ * https://blog.naver.com/eun1310434
+ */
+
+package com.eun1310434.tablelayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_input);
+    }
+}
