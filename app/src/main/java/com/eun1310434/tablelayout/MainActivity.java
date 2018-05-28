d473e82634dc9cded4cf6b72b7dd508e853c4fb5
eun1310434 <eun1310434@naver.com>
2018-05-28 11:04:12 +0200

    -

--- app/src/main/java/com/eun1310434/tablelayout/MainActivity.java
+++ app/src/main/java/com/eun1310434/tablelayout/MainActivity.java
@@ -1,8 +1,24 @@
-/**
- * 19.02.2018
- * eun1310434@naver.com
- * https://blog.naver.com/eun1310434
- */
+/*=====================================================================
+□ INFORMATION
+  ○ Data : 28.05.2018
+  ○ Mail : eun1310434@naver.com
+  ○ Blog : https://blog.naver.com/eun1310434
+  ○ Reference : Do it android app Programming
+
+□ FUNCTION
+  ○
+
+□ STUDY
+  ○ Table Layout
+     - A layout that arranges its children into rows and columns.
+       A TableLayout consists of a number of TableRow objects, each defining a row
+       (actually, you can have other children, which will be explained below).
+       TableLayout containers do not display border lines for their rows, columns, or cells.
+       Each row has zero or more cells; each cell can hold one View object.
+       The table has as many columns as the row with the most cells.
+       A table can leave cells empty.
+       Cells can span columns, as they can in HTML.
+=====================================================================*/
 
 package com.eun1310434.tablelayout;
 
